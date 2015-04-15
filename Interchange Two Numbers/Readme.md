70e4421ebc91d1b36f56c20017250a7e4e6fe545
Tatparya Shankar <tshankar@purdue.edu>
2015-04-15 15:44:03 -0400

    Interchange Two Numbers

---
+++ Interchange Two Numbers/Readme.md
@@ -0,0 +1,4 @@
+<h3>Interchange Two Numbers</h3>
+---
+Problem Link:<br/>
+https://www.hackerrank.com/challenges/interchange-two-numbers
---
+++ Interchange Two Numbers/solution.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    a = input()
+    b = input()
+    tup = (b,a)
+    for element in tup:
+        print( element )
+
+if __name__ == "__main__":
+    main()
