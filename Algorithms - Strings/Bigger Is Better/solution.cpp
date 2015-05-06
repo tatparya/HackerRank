856c244f336819eaef1f4e2c4d5b3b92077fc327
tatparya <tatparya.shankar@gmail.com>
2015-05-06 00:05:08 -0400

    Incomplete Bigger is better

--- Algorithms - Strings/Bigger Is Better/solution.cpp
+++ Algorithms - Strings/Bigger Is Better/solution.cpp
@@ -8,10 +8,17 @@ using namespace std;
 
 void exchangeChar( char * string, int i, int j )
 {
-    
+    char temp;
+
+    temp = string[i];
+    string[i] = string[j];
+    string[j] = temp;
 }
 
 void getResult()
+
+//  Get next min
+//  Srt
 {
     //  Get String
     char string[101];
@@ -19,18 +26,23 @@ void getResult()
     int found = 0;
     scanf( "%s", string );
     length = strlen( string );
-
+    
     //  Iterate over string
     for( int i = length - 1; i >= 0; i-- )
     {
-        for( int j = i; j >= 0; j++ )
+        for( int j = i - 1; j >= 0; j-- )
         {
+            //printf( "char : %d,%d %d,%d\n", string[i], i, string[j], j );
             if( string[i] > string[j] )
             {
                 exchangeChar( string, i, j );
+                printf( "%s\n", string );
+                return;
             }
         }
     }
+
+    printf( "no answer\n" );
 }
 
 int main() {
