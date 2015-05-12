0b9640b646974ac2f0f77237535092e77adad990
tatparya <tatparya.shankar@gmail.com>
2015-05-12 05:11:52 -0400

    Added alt chars

---
+++ Algorithms - Strings/Alternating Characters/solution.cpp
@@ -0,0 +1,52 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+void deleteNext( char * string, int i, int length )
+{
+    for( ; i < length - 1; i++ )
+    {
+        string[i] = string[i + 1];
+    }
+}
+
+void getResult()
+
+//  Get next min
+//  Srt
+{
+    //  Get String
+    char string[101];
+    int length;
+    int del = 0;
+    scanf( "%s", string );
+    length = strlen( string );
+    
+    //  Iterate over string
+    for( int i = length - 1; i >= 0; i-- )
+    {
+        if( string[i] == string[i+1] )
+        {
+            deleteNext( string, i + 1, length );
+            del++;
+        }
+    }
+
+    cout << del << endl;
+}
+
+int main() {
+    //  Get numTestCases
+    int numTestCases;
+    cin >> numTestCases;
+
+    for( int i = 0; i < numTestCases; i++ )
+    {
+        getResult();
+    }
+    return 0;
+}
