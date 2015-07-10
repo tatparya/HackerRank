0f4d8179226ff6819590513c06df29d819540cbf
tatparya <tatparya.shankar@gmail.com>
2015-07-10 17:00:13 -0400

    Solving Ice Cream Parlour

---
+++ Algorithms - Search/Ice Cream Parlour/Readme.md
@@ -0,0 +1,4 @@
+<h3>Ice Cream Parlor</h3>
+---
+Problem Link:<br/>
+https://www.hackerrank.com/challenges/icecream-parlor
---
+++ Algorithms - Search/Ice Cream Parlour/solution.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//	Author: Tatparya Shankar
+
+
+int main() 
+{
+	int n;
+	
+    return 0;
+}
