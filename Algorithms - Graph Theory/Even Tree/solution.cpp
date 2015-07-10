e89db538b13e47853d2a4af58b693a76cb15da0d
tatparya <tatparya.shankar@gmail.com>
2015-07-10 16:40:10 -0400

    Solved Even tree

--- Algorithms - Graph Theory/Even Tree/solution.cpp
+++ Algorithms - Graph Theory/Even Tree/solution.cpp
@@ -36,15 +36,15 @@ void Node::init( int nodeId, int parentId )
 {
 	id = nodeId;
 	parent = parentId;
-	cout << "Node Id : " << id + 1 << endl;
-	cout << "Parent : " << parent + 1 << endl;
+	// cout << "Node Id : " << id + 1 << endl;
+	// cout << "Parent : " << parent + 1 << endl;
 }
 
 void Node::addChild( int child )
 {
 	children.push_back( child );
 	this -> numChildren++;
-	cout << "Adding child : " << child + 1 << " to : " << this -> id + 1 << endl;
+	// cout << "Adding child : " << child + 1 << " to : " << this -> id + 1 << endl;
 }
 
 void Node::printChildren()
@@ -58,17 +58,29 @@ void Node::printChildren()
 	}
 }
 
-int getNumVertices( int n, Node nodes[ MAX ], int currentNode )
+int getNumVertices( int n, Node nodes[ MAX ], int currentNode, int &numCuts )
 {
+	
+	
 	if( nodes[currentNode].numChildren == 0 )
 	{
 		return 1;
 	}
 
-	for( int i = 0; i < nodes[i].numChildren; i++ )
+	for( int i = 0; i < nodes[currentNode].numChildren; i++ )
 	{
-		nodes[currentNode].numVertices += getNumVertices( n, nodes, nodes[currentNode].children[i] );
+		// cout << "numChildren : " << nodes[currentNode].numChildren << endl;
+		// cout << "Parent : " << currentNode + 1 << endl;
+		// cout << "Child : " << i + 1 << endl;
+		nodes[currentNode].numVertices += getNumVertices( n, nodes, nodes[currentNode].children[i], numCuts );
 	}
+
+	if( nodes[currentNode].numVertices % 2 == 0 )
+	{
+		numCuts++;
+	}
+
+	// cout << "numVertices : " << nodes[currentNode].numVertices << endl;
 
 	return nodes[currentNode].numVertices;
 }
@@ -80,6 +92,8 @@ int main()
 
 	int parent;
 	int child;
+
+	int numCuts = -1;
 
 	Node nodes[ 100 ];
 
@@ -99,15 +113,20 @@ int main()
 		nodes[parent - 1].addChild( child - 1 );
 	}
 
+	// cout <<"\n~~ GETTING VERTICES ~~ \n\n";
+
 	//	Calculate num vertices
-	getNumVertices( n, nodes, 0 );
+	getNumVertices( n, nodes, 0, numCuts );
 
-	//	Print all children
-	for( int i = 0; i < n; i++ )
-	{
-		nodes[i].printChildren();
-	}
+	// cout << "\n~~~ALL VERTICES~~~\n\n";
 
+	// //	Print all children
+	// for( int i = 0; i < n; i++ )
+	// {
+	// 	nodes[i].printChildren();
+	// }
+
+	cout << numCuts << endl;
 
     return 0;
 }
